ab57fb6b1d3eeb3869456ae1f9106027d25a2216
Alex <aamorgun2003@gmail.com>
2024-04-21 01:48:17 -0600

    Add body/main sections
    Added section wrapper (left column) to include
    a fun animation on load.
    
    The right column includes more information
    about the application and a button with the
    ability to sign up for testing. The right
    column also has an animation, which fires on
    load.
    
    Added the floc name to the navbar and made it
    slide in on load as well.
    
    Too many animation? I'm not sure.

--- src/DOM.js
+++ src/DOM.js
@@ -1,4 +1,8 @@
 const hamburger = document.querySelector(".hamburger-mobile");
 const mobileNav = document.querySelector(".mobile-nav");
+const bottomText = document.querySelector(".bottom-container h1");
+const topText = document.querySelector(".top-container h1");
+const logo = document.querySelector(".logo-word");
+const rightContainer = document.querySelector(".right-container");
 
-export { hamburger, mobileNav };
+export { hamburger, mobileNav, bottomText, topText, logo, rightContainer };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import "./style.css";
-import { hamburger, mobileNav } from "./DOM";
+import {
+  hamburger,
+  mobileNav,
+  bottomText,
+  topText,
+  logo,
+  rightContainer,
+} from "./DOM";
 import toggle from "./mobileNavigation";
 
 hamburger.addEventListener("click", () => {
   toggle(hamburger.firstElementChild, "line1");
   toggle(hamburger.lastElementChild, "line2");
   toggle(mobileNav, "mobile-nav-open");
+});
+
+window.addEventListener("load", () => {
+  toggle(topText, "top-show");
+  toggle(rightContainer, "slide-up-animation");
+  setTimeout(() => {
+    toggle(bottomText, "bottom-show");
+    toggle(logo, "show-logo");
+    // setTimeout(() => {
+    //   toggle(rightContainer, "slide-up-animation");
+    // }, 500);
+  }, 500);
 });
